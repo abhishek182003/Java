368399dc3a06949d152e04911effc41bd71eac72
abhishek182003 <97537083+abhishek182003@users.noreply.github.com>
2022-10-23 22:39:37 +0530

    Program to add any two in Java
    This is a simple program for adding two numbers on Java.

---
+++ sum.java
@@ -0,0 +1,11 @@
+class add {
+	public static void main(String args[])
+	{ 
+		int a=10,b=20,c=5;
+		System.out.println("a+b+c="+(a+b+c));
+		System.out.println("a+b="+(a+b));
+		System.out.println("a+c="+(a+c));
+		
+	}
+
+}
